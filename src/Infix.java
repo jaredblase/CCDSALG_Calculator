e381c78c4f9248a80f8917cdcd9c0ef9d733f34d
Jared Sy <jaredblase@gmail.com>
2020-08-19 01:35:16 +0800

    Added basic implementation

--- src/Infix.java
+++ src/Infix.java
@@ -1,5 +1,23 @@
 public class Infix {
     public static String toPostfix(String expression) {
-        return "Expression Converted";
+        Stack opStack = new Stack();
+        StringBuilder postfix = new StringBuilder();
+
+        // No concept of hierarchy yet
+        for(char i: expression.toCharArray()) {
+            if(Character.isDigit(i)) {
+                postfix.append(i);
+            } else {
+                postfix.append(" ");
+                opStack.push(i);
+            }
+        }
+
+        while(!opStack.isEmpty()) {
+            postfix.append(" ");
+            postfix.append(opStack.pop());
+        }
+
+        return postfix.toString();
     }
 }
