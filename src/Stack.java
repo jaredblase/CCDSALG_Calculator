a94a57631a307c228cd204241d5db374232cd496
teban <sc0531fy@gmail.com>
2020-08-18 14:24:09 +0800

    i try pushing - esteban

--- src/Stack.java
+++ src/Stack.java
@@ -1,2 +1,26 @@
+import java.ArrayList;
+
 public class Stack {
+
+
+	// Attributes
+	private int top = 0; 
+	private ArrayList<Int> stack = new ArrayList<int>();
+
+
+	public boolean isEmpty() {
+	}
+
+	public boolean isFull() {
+	}
+
+	public boolean push() {
+
+	}
+
+	public boolean pop() {
+		
+	}
+
+
 }
