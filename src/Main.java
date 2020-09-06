6d001a4a3e1c3531a5ed6ef4bac4a483ef63bca5
Jared Sy <jaredblase@gmail.com>
2020-09-06 14:05:24 +0800

    Removed printing of input equation

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
         str = input.nextLine();
         while (!str.equals("QUIT")) {
-            System.out.println(str);
             try {
                 Queue tokens = Infix.toPostfix(str);         // convert infix to postfix expression
                 for(String t : tokens) {                     // display elements in the queue
